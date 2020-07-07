cfd39a6abe7b62f4c736586a17aff84fdd9a6e92
huangz <zhangyuhang@souche.com>
2020-07-07 16:41:34 +0800

    feat: 新增rbd拖动示例

--- src/Router.jsx
+++ src/Router.jsx
@@ -12,11 +12,13 @@ import {
   GithubOutlined,
   CalendarOutlined,
   TableOutlined,
+  PaperClipOutlined,
 } from '@ant-design/icons';
 import Home from './pages/index';
 import Main from './pages/main';
 import About from './pages/about';
 import Table from './pages/table';
+import Dnd from './pages/dnd';
 
 export function MenuRouterView() {
   const location = useLocation();
@@ -30,6 +32,9 @@ export function MenuRouterView() {
       </Menu.Item>
       <Menu.Item key="/table" icon={<TableOutlined />}>
         <Link to="/table">表格</Link>
+      </Menu.Item>
+      <Menu.Item key="/dnd" icon={<PaperClipOutlined />}>
+        <Link to="/dnd">拖动</Link>
       </Menu.Item>
       <Menu.Item key="/main" icon={<GithubOutlined />}>
         <Link to="/main">关于</Link>
@@ -48,6 +53,9 @@ export function ContentRouterView() {
       </Route>
       <Route path="/table">
         <Table />
+      </Route>
+      <Route path="/dnd">
+        <Dnd />
       </Route>
       <Route path="/">
         <Home />
---
+++ src/pages/dnd/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <iframe
+      src="https://codesandbox.io/s/40p81qy7v0"
+      frameborder="0"
+      style={{ width: '100%', height: '100%' }}
+      sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+    ></iframe>
+  );
+}
