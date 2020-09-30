a48f65b6efd57af0a6230a4998a94927d23fbde3
huangz <zhangyuhang@souche.com>
2020-09-30 11:01:05 +0800

    fix: 固定vite版本

--- src/pages/dnd/index.jsx
+++ src/pages/dnd/index.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <iframe
       src="https://codesandbox.io/s/40p81qy7v0"
-      frameborder="0"
+      frameBorder="0"
       style={{ width: '100%', height: '100%' }}
       sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
     ></iframe>
